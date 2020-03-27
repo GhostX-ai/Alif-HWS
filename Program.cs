1d780ced6096423291c7657499a205d9f09f5c4e
GhostX <egamovkhurshed29@gmail.com>
2020-03-27 15:37:07 +0500

    HW2603

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW2603
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
